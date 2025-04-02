243af76c50efa81519caeec5c94a7107b0498f6d
Mingyi BAO <bmyjacks@outlook.com>
2025-04-02 09:19:06 +0800

    Change to RV32I

--- src/MainCPU.cpp
+++ src/MainCPU.cpp
@@ -5,10 +5,9 @@
 #include <cstdarg>
 #include <cstdio>
 #include <cstdlib>
+#include <elfio/elfio.hpp>
 #include <iostream>
 #include <string>
-
-#include <elfio/elfio.hpp>
 
 #include "BranchPredictor.h"
 #include "Cache.h"
@@ -33,10 +32,8 @@ BranchPredictor::Strategy strategy = BranchPredictor::Strategy::NT;
 BranchPredictor branchPredictor;
 Simulator simulator(&memory, &branchPredictor);
 
-int main(int argc, char** argv)
-{
-  if (!parseParameters(argc, argv))
-  {
+int main(int argc, char** argv) {
+  if (!parseParameters(argc, argv)) {
     printUsage();
     exit(-1);
   }
@@ -73,21 +70,18 @@ int main(int argc, char** argv)
 
   // Read ELF file
   ELFIO::elfio reader;
-  if (!reader.load(elfFile))
-  {
+  if (!reader.load(elfFile)) {
     fprintf(stderr, "Fail to load ELF file %s!\n", elfFile);
     return -1;
   }
 
-  if (verbose)
-  {
+  if (verbose) {
     printElfInfo(&reader);
   }
 
   loadElfToMemory(&reader, &memory);
 
-  if (verbose)
-  {
+  if (verbose) {
     memory.printInfo();
   }
 
@@ -99,8 +93,7 @@ int main(int argc, char** argv)
   simulator.initStack(stackBaseAddr, stackSize);
   simulator.simulate();
 
-  if (dumpHistory)
-  {
+  if (dumpHistory) {
     printf("Dumping history to dump.txt...\n");
     simulator.dumpHistory();
   }
@@ -111,115 +104,83 @@ int main(int argc, char** argv)
   return 0;
 }
 
-bool parseParameters(int argc, char** argv)
-{
+bool parseParameters(int argc, char** argv) {
   // Read Parameters
-  for (int i = 1; i < argc; ++i)
-  {
-    if (argv[i][0] == '-')
-    {
-      switch (argv[i][1])
-      {
-      case 'v':
-        verbose = 1;
-        break;
-      case 's':
-        isSingleStep = 1;
-        break;
-      case 'd':
-        dumpHistory = 1;
-        break;
-      case 'b':
-        if (i + 1 < argc)
-        {
-          std::string str = argv[i + 1];
-          i++;
-          if (str == "AT")
-          {
-            strategy = BranchPredictor::Strategy::AT;
-          }
-          else if (str == "NT")
-          {
-            strategy = BranchPredictor::Strategy::NT;
-          }
-          else if (str == "BTFNT")
-          {
-            strategy = BranchPredictor::Strategy::BTFNT;
-          }
-          else if (str == "BPB")
-          {
-            strategy = BranchPredictor::Strategy::BPB;
-          }
-          else
-          {
+  for (int i = 1; i < argc; ++i) {
+    if (argv[i][0] == '-') {
+      switch (argv[i][1]) {
+        case 'v':
+          verbose = 1;
+          break;
+        case 's':
+          isSingleStep = 1;
+          break;
+        case 'd':
+          dumpHistory = 1;
+          break;
+        case 'b':
+          if (i + 1 < argc) {
+            std::string str = argv[i + 1];
+            i++;
+            if (str == "AT") {
+              strategy = BranchPredictor::Strategy::AT;
+            } else if (str == "NT") {
+              strategy = BranchPredictor::Strategy::NT;
+            } else if (str == "BTFNT") {
+              strategy = BranchPredictor::Strategy::BTFNT;
+            } else if (str == "BPB") {
+              strategy = BranchPredictor::Strategy::BPB;
+            } else {
+              return false;
+            }
+          } else {
             return false;
           }
-        }
-        else
-        {
+          break;
+        default:
           return false;
-        }
-        break;
-      default:
-        return false;
       }
-    }
-    else
-    {
-      if (elfFile == nullptr)
-      {
+    } else {
+      if (elfFile == nullptr) {
         elfFile = argv[i];
-      }
-      else
-      {
+      } else {
         return false;
       }
     }
   }
-  if (elfFile == nullptr)
-  {
+  if (elfFile == nullptr) {
     return false;
   }
   return true;
 }
 
-void printUsage()
-{
+void printUsage() {
   printf("Usage: Simulator riscv-elf-file [-v] [-s] [-d] [-b param]\n");
   printf("Parameters: \n\t[-v] verbose output \n\t[-s] single step\n");
   printf("\t[-d] dump memory and register trace to dump.txt\n");
-  printf("\t[-b param] branch perdiction strategy, accepted param AT, NT, "
-    "BTFNT, BPB\n");
+  printf(
+      "\t[-b param] branch perdiction strategy, accepted param AT, NT, "
+      "BTFNT, BPB\n");
 }
 
-void printElfInfo(ELFIO::elfio* reader)
-{
+void printElfInfo(ELFIO::elfio* reader) {
   printf("==========ELF Information==========\n");
 
-  if (reader->get_class() == ELFCLASS32)
-  {
+  if (reader->get_class() == ELFCLASS32) {
     printf("Type: ELF32\n");
-  }
-  else
-  {
+  } else {
     printf("Type: ELF64\n");
   }
 
-  if (reader->get_encoding() == ELFDATA2LSB)
-  {
+  if (reader->get_encoding() == ELFDATA2LSB) {
     printf("Encoding: Little Endian\n");
-  }
-  else
-  {
+  } else {
     printf("Encoding: Large Endian\n");
   }
 
-  if (reader->get_machine() == EM_RISCV)
-  {
+  if (reader->get_machine() == EM_RISCV) {
     printf("ISA: RISC-V(0x%x)\n", reader->get_machine());
-  }
-  else
-  {
+  } else {
     dbgprintf("ISA: Unsupported(0x%x)\n", reader->get_machine());
     exit(-1);
   }
@@ -228,8 +189,7 @@ void printElfInfo(ELFIO::elfio* reader)
   printf("Number of Sections: %d\n", sec_num);
   printf("ID\tName\t\tAddress\tSize\n");
 
-  for (int i = 0; i < sec_num; ++i)
-  {
+  for (int i = 0; i < sec_num; ++i) {
     const ELFIO::section* psec = reader->sections[i];
 
     printf("[%d]\t%-12s\t0x%llx\t%lld\n", i, psec->get_name().c_str(),
@@ -240,8 +200,7 @@ void printElfInfo(ELFIO::elfio* reader)
   printf("Number of Segments: %d\n", seg_num);
   printf("ID\tFlags\tAddress\tFSize\tMSize\n");
 
-  for (int i = 0; i < seg_num; ++i)
-  {
+  for (int i = 0; i < seg_num; ++i) {
     const ELFIO::segment* pseg = reader->segments[i];
 
     printf("[%d]\t0x%x\t0x%llx\t%lld\t%lld\n", i, pseg->get_flags(),
@@ -252,21 +211,18 @@ void printElfInfo(ELFIO::elfio* reader)
   printf("===================================\n");
 }
 
-void loadElfToMemory(ELFIO::elfio* reader, MemoryManager* memory)
-{
+void loadElfToMemory(ELFIO::elfio* reader, MemoryManager* memory) {
   ELFIO::Elf_Half seg_num = reader->segments.size();
-  for (int i = 0; i < seg_num; ++i)
-  {
+  for (int i = 0; i < seg_num; ++i) {
     const ELFIO::segment* pseg = reader->segments[i];
 
     uint64_t fullmemsz = pseg->get_memory_size();
     uint64_t fulladdr = pseg->get_virtual_address();
     // Our 32bit simulator cannot handle this
-    if (fulladdr + fullmemsz > 0xFFFFFFFF)
-    {
+    if (fulladdr + fullmemsz > 0xFFFFFFFF) {
       dbgprintf(
-        "ELF address space larger than 32bit! Seg %d has max addr of 0x%lx\n",
-        i, fulladdr + fullmemsz);
+          "ELF address space larger than 32bit! Seg %d has max addr of 0x%lx\n",
+          i, fulladdr + fullmemsz);
       exit(-1);
     }
 
@@ -274,14 +230,10 @@ void loadElfToMemory(ELFIO::elfio* reader, MemoryManager* memory)
     uint32_t memsz = pseg->get_memory_size();
     uint32_t addr = (uint32_t)pseg->get_virtual_address();
 
-    for (uint32_t p = addr; p < addr + memsz; ++p)
-    {
-      if (p < addr + filesz)
-      {
+    for (uint32_t p = addr; p < addr + memsz; ++p) {
+      if (p < addr + filesz) {
         memory->setByteNoCache(p, pseg->get_data()[p - addr]);
-      }
-      else
-      {
+      } else {
         memory->setByteNoCache(p, 0);
       }
     }
--- src/Simulator.h
+++ src/Simulator.h
@@ -15,171 +15,160 @@
 #include "BranchPredictor.h"
 #include "MemoryManager.h"
 
-namespace RISCV
-{
-  const int REGNUM = 32;
-  extern const char* REGNAME[32];
-  typedef uint32_t RegId;
+namespace RISCV {
+const int REGNUM = 32;
+extern const char* REGNAME[32];
+typedef uint32_t RegId;
 
-  enum Reg
-  {
-    REG_ZERO = 0,
-    REG_RA = 1,
-    REG_SP = 2,
-    REG_GP = 3,
-    REG_TP = 4,
-    REG_T0 = 5,
-    REG_T1 = 6,
-    REG_T2 = 7,
-    REG_S0 = 8,
-    REG_S1 = 9,
-    REG_A0 = 10,
-    REG_A1 = 11,
-    REG_A2 = 12,
-    REG_A3 = 13,
-    REG_A4 = 14,
-    REG_A5 = 15,
-    REG_A6 = 16,
-    REG_A7 = 17,
-    REG_S2 = 18,
-    REG_S3 = 19,
-    REG_S4 = 20,
-    REG_S5 = 21,
-    REG_S6 = 22,
-    REG_S7 = 23,
-    REG_S8 = 24,
-    REG_S9 = 25,
-    REG_S10 = 26,
-    REG_S11 = 27,
-    REG_T3 = 28,
-    REG_T4 = 29,
-    REG_T5 = 30,
-    REG_T6 = 31,
-  };
+enum Reg {
+  REG_ZERO = 0,
+  REG_RA = 1,
+  REG_SP = 2,
+  REG_GP = 3,
+  REG_TP = 4,
+  REG_T0 = 5,
+  REG_T1 = 6,
+  REG_T2 = 7,
+  REG_S0 = 8,
+  REG_S1 = 9,
+  REG_A0 = 10,
+  REG_A1 = 11,
+  REG_A2 = 12,
+  REG_A3 = 13,
+  REG_A4 = 14,
+  REG_A5 = 15,
+  REG_A6 = 16,
+  REG_A7 = 17,
+  REG_S2 = 18,
+  REG_S3 = 19,
+  REG_S4 = 20,
+  REG_S5 = 21,
+  REG_S6 = 22,
+  REG_S7 = 23,
+  REG_S8 = 24,
+  REG_S9 = 25,
+  REG_S10 = 26,
+  REG_S11 = 27,
+  REG_T3 = 28,
+  REG_T4 = 29,
+  REG_T5 = 30,
+  REG_T6 = 31,
+};
 
-  enum InstType
-  {
-    R_TYPE,
-    I_TYPE,
-    S_TYPE,
-    SB_TYPE,
-    U_TYPE,
-    UJ_TYPE,
-  };
+enum InstType {
+  R_TYPE,
+  I_TYPE,
+  S_TYPE,
+  SB_TYPE,
+  U_TYPE,
+  UJ_TYPE,
+};
 
-  enum Inst
-  {
-    LUI = 0,
-    AUIPC = 1,
-    JAL = 2,
-    JALR = 3,
-    BEQ = 4,
-    BNE = 5,
-    BLT = 6,
-    BGE = 7,
-    BLTU = 8,
-    BGEU = 9,
-    LB = 10,
-    LH = 11,
-    LW = 12,
-    LD = 13,
-    LBU = 14,
-    LHU = 15,
-    SB = 16,
-    SH = 17,
-    SW = 18,
-    SD = 19,
-    ADDI = 20,
-    SLTI = 21,
-    SLTIU = 22,
-    XORI = 23,
-    ORI = 24,
-    ANDI = 25,
-    SLLI = 26,
-    SRLI = 27,
-    SRAI = 28,
-    ADD = 29,
-    SUB = 30,
-    SLL = 31,
-    SLT = 32,
-    SLTU = 33,
-    XOR = 34,
-    SRL = 35,
-    SRA = 36,
-    OR = 37,
-    AND = 38,
-    ECALL = 39,
-    ADDIW = 40,
-    MUL = 41,
-    MULH = 42,
-    DIV = 43,
-    REM = 44,
-    LWU = 45,
-    SLLIW = 46,
-    SRLIW = 47,
-    SRAIW = 48,
-    ADDW = 49,
-    SUBW = 50,
-    SLLW = 51,
-    SRLW = 52,
-    SRAW = 53,
-    UNKNOWN = -1,
-  };
+enum Inst {
+  LUI = 0,
+  AUIPC = 1,
+  JAL = 2,
+  JALR = 3,
+  BEQ = 4,
+  BNE = 5,
+  BLT = 6,
+  BGE = 7,
+  BLTU = 8,
+  BGEU = 9,
+  LB = 10,
+  LH = 11,
+  LW = 12,
+  LD = 13,
+  LBU = 14,
+  LHU = 15,
+  SB = 16,
+  SH = 17,
+  SW = 18,
+  SD = 19,
+  ADDI = 20,
+  SLTI = 21,
+  SLTIU = 22,
+  XORI = 23,
+  ORI = 24,
+  ANDI = 25,
+  SLLI = 26,
+  SRLI = 27,
+  SRAI = 28,
+  ADD = 29,
+  SUB = 30,
+  SLL = 31,
+  SLT = 32,
+  SLTU = 33,
+  XOR = 34,
+  SRL = 35,
+  SRA = 36,
+  OR = 37,
+  AND = 38,
+  ECALL = 39,
+  ADDIW = 40,
+  MUL = 41,
+  MULH = 42,
+  DIV = 43,
+  REM = 44,
+  LWU = 45,
+  SLLIW = 46,
+  SRLIW = 47,
+  SRAIW = 48,
+  ADDW = 49,
+  SUBW = 50,
+  SLLW = 51,
+  SRLW = 52,
+  SRAW = 53,
+  UNKNOWN = -1,
+};
 
-  extern const char* INSTNAME[];
+extern const char* INSTNAME[];
 
-  // Opcode field
-  const int OP_REG = 0x33;
-  const int OP_IMM = 0x13;
-  const int OP_LUI = 0x37;
-  const int OP_BRANCH = 0x63;
-  const int OP_STORE = 0x23;
-  const int OP_LOAD = 0x03;
-  const int OP_SYSTEM = 0x73;
-  const int OP_AUIPC = 0x17;
-  const int OP_JAL = 0x6F;
-  const int OP_JALR = 0x67;
-  const int OP_IMM32 = 0x1B;
-  const int OP_32 = 0x3B;
+// Opcode field
+const int OP_REG = 0x33;
+const int OP_IMM = 0x13;
+const int OP_LUI = 0x37;
+const int OP_BRANCH = 0x63;
+const int OP_STORE = 0x23;
+const int OP_LOAD = 0x03;
+const int OP_SYSTEM = 0x73;
+const int OP_AUIPC = 0x17;
+const int OP_JAL = 0x6F;
+const int OP_JALR = 0x67;
+const int OP_IMM32 = 0x1B;
+const int OP_32 = 0x3B;
 
-  inline bool isBranch(Inst inst)
-  {
-    if (inst == BEQ || inst == BNE || inst == BLT || inst == BGE ||
-      inst == BLTU || inst == BGEU)
-    {
-      return true;
-    }
-    return false;
+inline bool isBranch(Inst inst) {
+  if (inst == BEQ || inst == BNE || inst == BLT || inst == BGE ||
+      inst == BLTU || inst == BGEU) {
+    return true;
   }
+  return false;
+}
 
-  inline bool isJump(Inst inst)
-  {
-    if (inst == JAL || inst == JALR)
-    {
-      return true;
-    }
-    return false;
+inline bool isJump(Inst inst) {
+  if (inst == JAL || inst == JALR) {
+    return true;
   }
+  return false;
+}
 
-  inline bool isReadMem(Inst inst)
-  {
-    if (inst == LB || inst == LH || inst == LW || inst == LD || inst == LBU ||
-      inst == LHU || inst == LWU)
-    {
-      return true;
-    }
-    return false;
+inline bool isReadMem(Inst inst) {
+  if (inst == LB || inst == LH || inst == LW || inst == LD || inst == LBU ||
+      inst == LHU || inst == LWU) {
+    return true;
   }
-} // namespace RISCV
+  return false;
+}
+}  // namespace RISCV
 
-class Simulator
-{
-public:
+class Simulator {
+ public:
   bool isSingleStep;
   bool verbose;
   bool shouldDumpHistory;
-  uint64_t pc;
-  uint64_t reg[RISCV::REGNUM];
+  uint32_t pc;
+  uint32_t reg[RISCV::REGNUM];
   uint32_t stackBase;
   uint32_t maximumStackSize;
   MemoryManager* memory;
@@ -198,49 +187,47 @@ public:
 
   void printStatistics();
 
-private:
-  struct FReg
-  {
+ private:
+  struct FReg {
     // Control Signals
     bool bubble;
     uint32_t stall;
 
-    uint64_t pc;
+    uint32_t pc;
     uint32_t inst;
     uint32_t len;
   } fReg, fRegNew;
 
-  struct DReg
-  {
+  struct DReg {
     // Control Signals
     bool bubble;
     uint32_t stall;
     RISCV::RegId rs1, rs2;
 
-    uint64_t pc;
+    uint32_t pc;
     RISCV::Inst inst;
-    int64_t op1;
-    int64_t op2;
+    int32_t op1;
+    int32_t op2;
     RISCV::RegId dest;
-    int64_t offset;
+    int32_t offset;
     bool predictedBranch;
-    uint64_t predictedPC; // for branch prediction module, predicted PC destination
-    uint64_t anotherPC; // another possible prediction destination
+    uint32_t
+        predictedPC;  // for branch prediction module, predicted PC destination
+    uint32_t anotherPC;  // another possible prediction destination
   } dReg, dRegNew;
 
-  struct EReg
-  {
+  struct EReg {
     // Control Signals
     bool bubble;
     uint32_t stall;
 
-    uint64_t pc;
+    uint32_t pc;
     RISCV::Inst inst;
-    int64_t op1;
-    int64_t op2;
+    int32_t op1;
+    int32_t op2;
     bool writeReg;
     RISCV::RegId destReg;
-    int64_t out;
+    int32_t out;
     bool writeMem;
     bool readMem;
     bool readSignExt;
@@ -248,17 +235,16 @@ private:
     bool branch;
   } eReg, eRegNew;
 
-  struct MReg
-  {
+  struct MReg {
     // Control Signals
     bool bubble;
     uint32_t stall;
 
-    uint64_t pc;
+    uint32_t pc;
     RISCV::Inst inst;
-    int64_t op1;
-    int64_t op2;
-    int64_t out;
+    int32_t op1;
+    int32_t op2;
+    int32_t out;
     bool writeReg;
     RISCV::RegId destReg;
   } mReg, mRegNew;
@@ -270,14 +256,14 @@ private:
   bool memoryWriteBack;
   RISCV::RegId memoryWBReg;
 
-  struct History
-  {
+  struct History {
     uint32_t instCount;
     uint32_t cycleCount;
     uint32_t stalledCycleCount;
 
-    uint32_t predictedBranch; // Number of branch that is predicted successfully
-    uint32_t unpredictedBranch; // Number of branch that is not predicted
+    uint32_t
+        predictedBranch;  // Number of branch that is predicted successfully
+    uint32_t unpredictedBranch;  // Number of branch that is not predicted
     // successfully
 
     uint32_t dataHazardCount;
@@ -296,7 +282,7 @@ private:
   void memoryAccess();
   void writeBack();
 
-  int64_t handleSystemCall(int64_t op1, int64_t op2);
+  int32_t handleSystemCall(int32_t op1, int32_t op2);
 
   std::string getRegInfoStr();
   void panic(const char* format, ...);
